4fd800f55647e27d8c6fe7a7649ca81c7e2fe721
Dnmrk4 <danmark.chemuren@gmail.com>
2019-08-28 10:18:32 +0300

    photo urls

---
+++ photos/urls.py
@@ -0,0 +1,16 @@
+from django.conf.urls import url
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns=[
+    url('^$',views.index,name = 'index'),
+    url(r'^search/', views.search_results, name='search_results'),
+    url(r'^nairobi/', views.nairobi, name='nairobi'),
+    url(r'^rio/', views.rio, name='rio'),
+    url(r'^tokyo/', views.tokyo, name='tokyo'),
+    url(r'^berlin/', views.berlin, name='berlin'),
+]
+
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
